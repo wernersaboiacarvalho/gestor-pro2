c6dbc1d532405d6e877b649c0322359cbd145acc
Werner Saboia <wernersaboia@gmail.com>
2026-05-27 11:46:53 -0300

    fix: resolvendo problema com Prisma

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -3,7 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  max: 5,
+})
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
